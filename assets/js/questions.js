a628bd233f60725fb9218faa665bca5097d14e0c
Alek Seawright <alek@swalek.local>
2020-01-06 23:26:03 -0600

    question text and choice buttons display now.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,19 +2,66 @@
 var timeLeft = document.querySelector(".timer");
 
 // set time limit
-var secondsLeft = 5; // instead, say something like 15seconds*number of questions in array = time limit
-
+var secondsLeft = 30; // instead, say something like 15seconds*number of questions in array = time limit?
 function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeLeft.textContent = secondsLeft + " seconds remaining";
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeLeft.textContent = secondsLeft + " seconds remaining";
 
-    if(secondsLeft <= 0) {
-      clearInterval(timerInterval);
-      timeLeft.textContent = "Time's up!";
+        // when time runs out..
+        if(secondsLeft <= 0) {
+        clearInterval(timerInterval);
+        timeLeft.textContent = "Time's up!";
     }
   },1000);
+} // end setTime function
+
+// create array of questions/answers
+var questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      question: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    }
+  ]; //end questions array
+
+
+// create global variables for question, choices, answers
+let currentQuestion = questions[0].question;
+let choices = questions[0].choices;
+let answer = questions[0].answer;
+
+// display choices as buttons
+function displayChoices() {
+  for (var i = 0; i < choices.length; i++) {
+    var answers = document.createElement("button")
+    answers.innerHTML = choices[i]
+    document.getElementById("choicesText").appendChild(answers);
+  };
 }
+
+// clear the screen
+function clearText() {
+  document.getElementById('headerText').innerHTML = currentQuestion;
+  document.getElementById('choicesText').innerHTML = "";
+}
+
+
+
+
+    // display question and choices (buttons) in html
+    // add on click event to choices that moves to next question and:
+        // compare clicked answer to correct answer
+        // display right/wrong
+        // if wrong, subtract 10s from timeLeft
+    // final question has submit button
+        // on click stores timeLeft to local storage
+        // displays end page
 
 // function timeUp() {
 //   timeLeft.textContent = "Time's up!";
@@ -25,5 +72,3 @@ function setTime() {
 // //   mainEl.appendChild(imgEl);
 
 // }
-
-//setTime(); // end create timer
