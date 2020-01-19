1110c498ed735f56d226b599732446d278cac2af
Alek Seawright <alek@swalek.local>
2020-01-19 10:00:17 -0600

    new questions now appear, timer keeps accurate track of score, created name entry field and submit button

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,10 +1,13 @@
+$(document).ready(function() {
+
+
 // create timer
 var timeLeft = document.querySelector(".timer");
-
+var timerInterval;
 // set time limit
 var secondsLeft = 75; // instead, say something like 15seconds*number of questions in array = time limit?
 function setTime() {
-    var timerInterval = setInterval(function() {
+     timerInterval = setInterval(function() { //assignment
         secondsLeft--;
         timeLeft.textContent = secondsLeft + " seconds remaining";
 
@@ -43,26 +46,90 @@ var questions = [
   ]; //end questions array
 
 
+//hide score form
+let scoreForm = document.getElementById('scoreForm');
+scoreForm.style.visibility = 'hidden';
+
+
 // create global variables for question, choices, answers
+
+let questionIndex = 0
 let currentQuestion = questions[0].question;
 let choices = questions[0].choices;
 let answer = questions[0].answer;
 
+function updateGlobals(){
+ currentQuestion = questions[questionIndex].question;
+ choices = questions[questionIndex].choices;
+ answer = questions[questionIndex].answer;
+
+ displayChoices()
+}
+
 // display choices as buttons
 function displayChoices() {
+  clearText()  
   for (var i = 0; i < choices.length; i++) {
+    // setup answer choice buttons
     var answers = document.createElement("btn-group-vertical")
-    answers.className="btn btn-light "
-    answers.innerHTML = choices[i]
+    answers.className="btn btn-light"
+
+    //remove submit button
+    let scoreBtn = document.getElementById('submit-btn')
+    scoreBtn.style.visibility = 'hidden';
+
+    // on click events
+    answers.innerText = choices[i]
     document.getElementById("choicesText").appendChild(answers);
+    answers.addEventListener('click', function(event) {
+      event.preventDefault()
+      let userAnswer = this.innerText;
+      if(userAnswer===answer){
+
+      }else{
+        secondsLeft = secondsLeft - 10;
+      }
+      if(questionIndex==questions.length-1){
+        gameOver()
+      }else{
+        //advance   
+        questionIndex++
+        updateGlobals()
+      }
+    })
   };
+}
+function gameOver(){
+  // stop time
+  clearInterval(timerInterval);
+  
+  // display game over in 'time remaining' field
+  timeLeft.innerText = 'Game over!';
+
+  //pull current clock value as score
+  let finalScore = secondsLeft;
+
+  //display the score and input fields
+  document.getElementById('headerText').innerHTML = `Your score is ${finalScore}`;
+  document.getElementById('choicesText').innerHTML = "";
+  scoreForm.style.visibility = 'visible';
+
 }
 
 // clear the screen
 function clearText() {
   document.getElementById('headerText').innerHTML = currentQuestion;
   document.getElementById('choicesText').innerHTML = "";
+  document.getElementsByClassName('btn-primary').innerHTML = "";
 }
+
+// create on-click event attached to start quiz button
+$('.start-btn').on('click', function(event) {
+  event.preventDefault()
+    setTime(); displayChoices(); //displayQuestion();
+});
+
+});
 
 
 
